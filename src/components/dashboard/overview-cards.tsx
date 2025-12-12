bbad074cfe95ac9292ac9bbdf68c2c4745c439f5
Lucas Cavalcante <LucasCavalcante102@gmail.com>
2025-12-12 05:08:25 +0000

    Na Dashboard, aplique cores nos Cards

--- src/components/dashboard/overview-cards.tsx
+++ src/components/dashboard/overview-cards.tsx
@@ -6,6 +6,7 @@ import { ArrowUp, ArrowDown, Scale } from "lucide-react";
 import { Transaction } from "@/lib/types";
 import { useMemo } from "react";
 import { Skeleton } from "../ui/skeleton";
+import { cn } from "@/lib/utils";
 
 const formatCurrency = (amount: number) => {
   return new Intl.NumberFormat('en-US', {
@@ -56,20 +57,20 @@ export function OverviewCards({ transactions, isLoading }: OverviewCardsProps) {
 
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-      <Card>
+      <Card className="bg-green-600 text-white">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Total Income</CardTitle>
-          <ArrowUp className="h-4 w-4 text-green-500" />
+          <ArrowUp className="h-4 w-4" />
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">{formatCurrency(totalIncome)}</div>
           {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
         </CardContent>
       </Card>
-      <Card>
+      <Card className="bg-red-600 text-white">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Total Expenses</CardTitle>
-          <ArrowDown className="h-4 w-4 text-red-500" />
+          <ArrowDown className="h-4 w-4" />
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">{formatCurrency(totalExpenses)}</div>
@@ -82,7 +83,7 @@ export function OverviewCards({ transactions, isLoading }: OverviewCardsProps) {
           <Scale className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">{formatCurrency(balance)}</div>
+          <div className={cn("text-2xl font-bold", balance < 0 ? 'text-destructive' : 'text-green-600')}>{formatCurrency(balance)}</div>
           <p className="text-xs text-muted-foreground">Your account balance this month</p>
         </CardContent>
       </Card>
