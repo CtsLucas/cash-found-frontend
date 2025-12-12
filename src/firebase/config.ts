f547a210c0a8866b0676069c9aa4860622593d92
Lucas Cavalcante <LucasCavalcante102@gmail.com>
2025-12-12 02:08:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4679614369-ef96b",
+  "appId": "1:481529069052:web:c0dd77f92dba5e535a0679",
+  "apiKey": "AIzaSyADKbC9BN_bG3DgMtEBqU8Y__x3Vn4QQRw",
+  "authDomain": "studio-4679614369-ef96b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "481529069052"
+};
