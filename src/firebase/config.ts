4f04138af912677cf3c7315be102556612188a9c
lucascavalcante102 <lucascavalcante102@gmail.com>
2025-12-13 00:39:59 +0000

    ci: update firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4679614369-ef96b",
-  "appId": "1:481529069052:web:c0dd77f92dba5e535a0679",
-  "apiKey": "AIzaSyADKbC9BN_bG3DgMtEBqU8Y__x3Vn4QQRw",
-  "authDomain": "studio-4679614369-ef96b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "481529069052"
+  apiKey: "AIzaSyAKNYJpLiX9xcpNwte7OcCSa0v5cDidXCM",
+  authDomain: "cash-found-frontend.firebaseapp.com",
+  projectId: "cash-found-frontend",
+  storageBucket: "cash-found-frontend.firebasestorage.app",
+  messagingSenderId: "403613068568",
+  appId: "1:403613068568:web:1f413f8f1a2978fff2426d"
 };
